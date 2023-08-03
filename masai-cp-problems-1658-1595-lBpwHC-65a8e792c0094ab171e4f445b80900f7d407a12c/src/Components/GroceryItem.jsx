3510820af2d99288c2f8752e7f494fafe341b0fb
server_42 <devbhattacharya42@gmail.com>
2023-08-03 22:26:21 +0530

    Button functionality is left

--- masai-cp-problems-1658-1595-lBpwHC-65a8e792c0094ab171e4f445b80900f7d407a12c/src/Components/GroceryItem.jsx
+++ masai-cp-problems-1658-1595-lBpwHC-65a8e792c0094ab171e4f445b80900f7d407a12c/src/Components/GroceryItem.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import styles from './groceryItem.module.css'
+const GroceryItem = ({props}) => {
 
-const GroceryItem = () => {
   return (
-    <div></div>
+    <div className={styles.groceryItem}>
+      <img src={props.image} alt="Groceries" className={styles.img}/>
+      <h3 className={styles.itemName}>{props.name}</h3>
+      <h5 className={styles.itemPrice}>₹{props.price}</h5>
+      <button data-cy="add_to_cart" className={styles.btn}>Add to cart</button>
+    </div>
   )
 }
 
